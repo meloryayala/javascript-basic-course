08f42d8e471629c26a25cd5be4685e4e91071fc2
Melory Ayala <melory.ayala@gmail.com>
2020-04-20 09:27:51 +0200

    Anatomy

---
+++ 3-js-anatomy/script.js
@@ -0,0 +1,31 @@
+
+
+//comment 1 line//
+
+/*comment multiple 
+lines
+*/
+
+//JS Statements- composto de valores, operadores, expressoes, palavras-chave e comentarios.//
+
+/* palavras chaves
+
+
+break
+continue
+debugger
+do...while
+for
+function
+if...
+return
+switch
+try
+var
+let
+const
+*/
+
+console.log('hello world');
+console.log('hello world 2');
+
