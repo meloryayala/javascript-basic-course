9818d0da575f2e792d8cb13d8b9debdb521eb233
Melory Ayala <melory.ayala@gmail.com>
2020-04-21 09:29:20 +0200

    Variables

---
+++ 4-variables/script.js
@@ -0,0 +1,31 @@
+
+// 3 tipos de escrever variavel
+
+// tipos -> var, let, const
+
+//porém o (var) esta caindo em desuso desde que chegou o (let) e o (const)
+
+
+
+//esse tipo de dado é string e precisa '' 
+//let name='Meli'
+
+//esse tipo de dado é number e nao precisa '' e o pc pode fazer contas
+//let age= 25
+
+
+
+//(let) é uma declaração que pode ser reescrita/sobreescrita
+//let name='Meli'
+//name="Fulano"
+
+//(const) é uma declaração que nao pode ser reescrita, por isso é uma constante
+//const nameComplete_person='Meli' camel case or underline
+//variaveis nao podem começar com numero, mas com underline pode iniciar
+//variavei podem se escrever tudo em uppercase
+
+const name='Meli'
+
+let age=25
+
+console.log(name)
