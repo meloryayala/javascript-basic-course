dfe0f298dccb559a5d5392bc8ad7d1d1e9f5e2a3
Melory Ayala <melory.ayala@gmail.com>
2020-04-23 09:30:21 +0200

    DataTypes

---
+++ 5-dataTypes/script.js
@@ -0,0 +1,81 @@
+
+/*tipos de dados
+
+string
+number
+array
+object
+undefined
+null
+*/
+
+
+//string
+    let name='Meli'
+    console.log(name)
+
+    //string - concatenada
+    let lastName='Ayala'
+    //console.log(name + ' ' + lastName)
+
+    //string - literal
+    console.log(`${name} ${lastName}`)
+
+
+
+//number    
+    let age= 25
+    console.log(age+10)
+
+    age='25'
+    console.log(age+10)
+
+
+    //number float
+    let percentage= 10.2
+    console.log(percentage+ '%')
+
+
+//boolean (true or false)
+
+    let overAge = true
+    console.log(overAge)
+
+    let underAge= false
+    console.log(underAge)
+
+
+//array - caixinha com varias coisas
+
+    let skills = ['Javascript', 'PHP', 'Phyton']
+    console.log(skills)
+
+    console.log(skills.length)
+    console.log(skills[0])
+
+
+//object -caixinha tbm com diferentes conteudos
+
+let person = {
+    name: 'Meli',
+    lastName: 'Ayala',
+    age: 25,
+    skills: ['Javascript', 'PHP', 'Phyton']
+}
+
+    console.log(person)
+    console.log(person.lastName)
+
+
+      //JSON -> (Javascript object notation)
+
+
+//undefined
+
+    let adress
+    console.log(adress)
+
+//null
+
+    let city= null
+    
