3dc92c43ec87175a6a9bfbe06330f3ce6ee7dc70
Melory Ayala <melory.ayala@gmail.com>
2020-05-04 09:31:30 +0200

    Operators

---
+++ 6-operators/script.js
@@ -0,0 +1,86 @@
+
+/*tipos de operadores
+
+adição: +
+subtração: -
+multiplicação: *
+divisão: /
+incremento: ++
+decremento: --
+
+atribuição: += -=
+
+*/
+
+
+
+//adição
+
+    let a= 10
+    let b
+    let c= a + b
+
+    console.log(c)
+
+
+    //NaN = Not a Number
+
+
+//subtração
+
+    let d= 112
+    let e= 4
+    let f= d - e
+
+    console.log(f)
+
+
+//multiplicação
+
+    let g= 10
+    let h= 5
+    let i= g * h
+
+    console.log(i)
+
+
+//divisão
+
+    let j= 40
+    let k= 6
+    let l= j / k
+
+    console.log(l)
+
+
+//imcremento
+
+    let m=10
+    m++
+
+    console.log(m)
+
+
+//imcremento
+
+    let n=10
+    n--
+
+    console.log(n)
+
+
+//atribuição
+
+    let o = 12
+    //o = o + 25
+    o += 25
+
+    console.log(o)
+
+
+    let p = 12
+    //p = p - 25
+    p -= 25
+
+    console.log(p)
+
