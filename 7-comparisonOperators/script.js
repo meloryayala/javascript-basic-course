7e025b5d94531925723b7a4c1cea6a2b49d64e65
Melory Ayala <melory.ayala@gmail.com>
2020-05-07 09:35:58 +0200

    Comparison Operators

---
+++ 7-comparisonOperators/script.js
@@ -0,0 +1,84 @@
+
+/*tipos de operadores
+
+== igualdade valores
+
+=== igualdade valores e tipos
+
+!= diferená de valores
+
+!== diferença valores e tipos
+
+> maior que
+
+< menor que
+
+>= maior ou igual
+
+<= menor ou igual
+
+*/
+
+
+
+//igualdade valores (==)
+
+    let a = 10
+    let b = 5
+
+    console.log(a==b)
+
+
+//igualdade valores e tipos (===)
+    //iremos usar === a maior parte das vezes
+
+    let c=10
+    let d='10'
+    console.log(c===d)
+
+    console.log(c==d)
+
+
+//diferença entre valores (!=)
+
+    let e=10
+    let f='10'
+    console.log(c!=d)
+
+
+// diferença entre valores e tipos (!==)
+
+    let g=10
+    let h='10'
+    console.log(c!==d)
+
+
+//maior que (>)
+
+    let i=10
+    let j=5
+
+    console.log(i>j)
+
+//menor que (<)
+
+    let k=10
+    let l=5
+
+    console.log(k<l)
+
+
+// maior ou igual (>=)
+
+    let m=10
+    let n=10
+
+    console.log(m>=n)
+
+
+// menor ou igual (<=)
+
+    let o=5
+    let p=4
+
+    console.log(p<=p)
