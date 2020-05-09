d46eca96c8f3b673f3320e6311b70c1773c5f4fe
Melory Ayala <melory.ayala@gmail.com>
2020-05-09 09:36:45 +0200

    Conditionals

---
+++ 8-Conditional/script.js
@@ -0,0 +1,21 @@
+
+/*
+
+let age= prompt('Digite sua idade')
+
+if (age >= 18) {
+    document.write('Ok, você é maior de idade')
+} else if (age===15) {
+    document.write('Você tem 15 anos!')
+}
+else {
+    document.write('Você não pode acessar!')
+}
+
+*/
+
+let nome = prompt('Digite seu nome')
+
+if(nome==='Meli' || nome=== 'meli') {
+    document.write (`Olá ${nome}, seja bem vindo!`)
+} else { document.write('Você não tem acesso de administrador')}
