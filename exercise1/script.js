a833e810b8b5541d77cb80d4d9e4e8834aa0f0fe
Melory Ayala <melory.ayala@gmail.com>
2020-05-24 09:44:57 +0200

    JS challenge 1

---
+++ exercise1/script.js
@@ -0,0 +1,13 @@
+
+
+let name = prompt ('Digite seu nome')
+
+let age = prompt ('Qual é a sua idade?')
+
+
+if (age < 18) {
+    document.write(`Olá ${name}, Você é menor de idade!`)
+} else if (age >= 18) { 
+    document.write(`Olá ${name}, você é maior de idade!`)
+}
+
